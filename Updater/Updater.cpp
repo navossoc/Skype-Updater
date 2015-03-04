4e561efa314118ee64c8d53fe8b8db7294adf56e
Rafael Cossovan <navossoc@gmail.com>
2015-03-03 22:39:34 -0300

    Added code to hook ShowWindow to prevent the update window from being displayed.

--- Updater/Updater.cpp
+++ Updater/Updater.cpp
@@ -5,6 +5,12 @@
 #include "Updater.h"
 
 //////////////////////////////////////////////////////////////////////////
+// Function pointers
+//////////////////////////////////////////////////////////////////////////
+
+prtShowWindow fnShowWindow = NULL;
+
+//////////////////////////////////////////////////////////////////////////
 // DLL events
 //////////////////////////////////////////////////////////////////////////
 
@@ -12,6 +18,19 @@ BOOL ProcessAttach()
 {
 	// Initialize
 	if (MH_Initialize() != MH_OK)
+	{
+		return FALSE;
+	}
+
+	// Create a hook for ShowWindow
+	if (MH_CreateHook(&ShowWindow, &DetourShowWindow,
+		reinterpret_cast<void**>(&fnShowWindow)) != MH_OK)
+	{
+		return FALSE;
+	}
+
+	// Enable all hooks
+	if (MH_EnableHook(MH_ALL_HOOKS) != MH_OK)
 	{
 		return FALSE;
 	}
@@ -28,4 +47,24 @@ BOOL ProcessDetach()
 	}
 
 	return TRUE;
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Detour functions
+//////////////////////////////////////////////////////////////////////////
+
+_Use_decl_annotations_
+BOOL WINAPI DetourShowWindow(HWND hWnd, int nCmdShow)
+{
+	WCHAR szClassName[128] = { 0 };
+
+	if (GetClassName(hWnd, szClassName, 127))
+	{
+		if (wcscmp(szClassName, SKYPE_UPDATE_CLASS) == 0)
+		{
+			DestroyWindow(hWnd);
+		}
+	}
+
+	return fnShowWindow(hWnd, nCmdShow);
 }
