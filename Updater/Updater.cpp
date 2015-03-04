8bd48c527d60fe0a7f13c25efc4a7dc3071c08ec
Rafael Cossovan <navossoc@gmail.com>
2015-03-03 22:33:29 -0300

    Added code to handle DLL_PROCESS_ATTACH and DLL_PROCESS_DETACH events.

--- Updater/Updater.cpp
+++ Updater/Updater.cpp
@@ -2,5 +2,30 @@
 //
 
 #include "stdafx.h"
+#include "Updater.h"
 
+//////////////////////////////////////////////////////////////////////////
+// DLL events
+//////////////////////////////////////////////////////////////////////////
 
+BOOL ProcessAttach()
+{
+	// Initialize
+	if (MH_Initialize() != MH_OK)
+	{
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+BOOL ProcessDetach()
+{
+	// Uninitialize
+	if (MH_Uninitialize() != MH_OK)
+	{
+		return FALSE;
+	}
+
+	return TRUE;
+}
--- Updater/dllmain.cpp
+++ Updater/dllmain.cpp
@@ -1,5 +1,6 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "stdafx.h"
+#include "Updater.h"
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -9,10 +10,9 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
+		return ProcessAttach();
 	case DLL_PROCESS_DETACH:
-		break;
+		return ProcessDetach();
 	}
 	return TRUE;
 }
